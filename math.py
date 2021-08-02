917a4c5ce917709da18f5c00d55546c7b8efab1b
Pratiksha1999 <57611570+Pratiksha1999@users.noreply.github.com>
2021-08-02 15:08:54 +0530

    Implemented subtract() on remote branch

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 #Subtract implementation
 def subtract(x,y):
-    return x-y			#Branch - Bug456
+    return x-y			#Branch - Bug456_remote
 # Multiply implementation
 def multiply(x,y):
     pass
